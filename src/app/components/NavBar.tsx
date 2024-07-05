dc6d80bfcdc86a399c8db45115095e8992baa62a
CamiloMaria <camilocraft08@gmail.com>
2024-07-05 16:24:18 -0400

    feat: Improve NavBar component with search functionality and responsive design

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -12,7 +12,7 @@ const NavBar = () => {
                 </Link>
             </div>
 
-            <div className="hidden md:flex w-[50%] text-sm">
+            <div className="hidden md:flex w-[50%] text-sm items-center justify-between">
                 <div className="flex gap-6 text-gray-600">
                     <Link href="/" className="flex gap-2 items-center">
                         <Image src="/home.png" alt="Homepage" width={16} height={16} className="w-4 h-4" />
@@ -27,6 +27,10 @@ const NavBar = () => {
                         <span>Stories</span>
                     </Link>
                 </div>
+                <div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-xl">
+                    <input type="text" placeholder="search..." className="bg-transparent outline-none" />
+                    <Image src="/search.png" alt="Search" width={14} height={14} />
+                </div>
             </div>
 
             <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
@@ -36,7 +40,7 @@ const NavBar = () => {
                 <ClerkLoaded>
                     <SignedIn>
                         <div className="cursor-pointer">
-                            <Image src="/people.png" alt="Profile" width={20} height={20} />
+                            <Image src="/people.png" alt="Profile" width={24} height={24} />
                         </div>
                         <div className="cursor-pointer">
                             <Image src="/messages.png" alt="Messages" width={20} height={20} />
